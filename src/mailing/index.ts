6919b822f569c082e78b8c74550fe0d506926a93
Scott Daniel <scottberry91@gmail.com>
2018-12-10 15:10:16 +0000

    0.0.5

--- src/mailing/index.ts
+++ src/mailing/index.ts
@@ -1,75 +1,75 @@
-import { createTransport, SendMailOptions } from 'nodemailer';
-import { logger } from '../logging';
-import { createToken } from '..';
+import { createTransport, SendMailOptions } from 'nodemailer'
+import { logger } from '../logging'
+import { createToken } from '..'
 
 interface TransportAuth {
-	user: string;
-	pass: string;
+    user: string
+    pass: string
 }
 
 interface TransportArgs {
-	host: string;
-	port: string;
-	auth: TransportAuth;
+    host: string
+    port: string
+    auth: TransportAuth
 }
 
 interface TokenArgs {
-	id: string | number;
-	username: string;
-	email: string;
+    id: string | number
+    username: string
+    email: string
 }
 
 export const generateEmailLink = async (
-	{ id, username, email }: TokenArgs,
-	clientUrl: string
+    { id, username, email }: TokenArgs,
+    clientUrl: string,
 ) => {
-	try {
-		const [ token ]: string[] = await createToken(
-			{ id, username, email },
-			'secret',
-			'7d'
-		);
+    try {
+        const [token]: string[] = await createToken(
+            { id, username, email },
+            'secret',
+            '7d',
+        )
 
-		const link: string = `${clientUrl}?t=${token}`;
+        const link: string = `${clientUrl}?t=${token}`
 
-		return link;
-	} catch (error) {
-		logger('Generate Email Confirmation').error({
-			level: '5',
-			message: error
-		});
+        return link
+    } catch (error) {
+        logger('Generate Email Confirmation').error({
+            level: '5',
+            message: error,
+        })
 
-		return error;
-	}
-};
+        return error
+    }
+}
 
 export const SendMail = (
-	{ host, port, auth }: TransportArgs,
-	email: SendMailOptions
+    { host, port, auth }: TransportArgs,
+    email: SendMailOptions,
 ): boolean => {
-	try {
-		const transport = createTransport(
-			// sgTransport({
-			//   auth: {
-			//     api_key: SENGRID_API
-			//   }
-			// })
-			{
-				host,
-				port,
-				auth
-			} as any
-		);
+    try {
+        const transport = createTransport(
+            // sgTransport({
+            //   auth: {
+            //     api_key: SENGRID_API
+            //   }
+            // })
+            {
+                host,
+                port,
+                auth,
+            } as any,
+        )
 
-		transport.sendMail(email);
+        transport.sendMail(email)
 
-		return true;
-	} catch (error) {
-		logger('Email').error({ level: '5', message: error });
+        return true
+    } catch (error) {
+        logger('Email').error({ level: '5', message: error })
 
-		return error;
-	}
-};
+        return error
+    }
+}
 
 // const {
 // 	EMAIL_HOST = 'smtp.mailtrap.io',
