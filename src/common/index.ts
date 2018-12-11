d03f4816b337bc5c307ae731afd5a3adc3ed6917
Scott Daniel <scottberry91@gmail.com>
2018-12-11 14:41:50 +0000

    minor changes

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,41 +1,32 @@
-import winston = require('winston')
-import * as glob from 'glob'
-import { IResolvers } from 'graphql-tools/dist/Interfaces'
+import winston = require('winston');
+import * as glob from 'glob';
+import { IResolvers } from 'graphql-tools/dist/Interfaces';
 
-import { logger } from '..'
+import { logger } from '..';
 
-export const sluggify = (word: string): string =>
-    word.replace(/ /g, '-').toLowerCase()
+export const sluggify = (word: string): string => word.replace(/ /g, '-').toLowerCase();
 
 export const normalisePort = (port: number | string): number => {
-    if (typeof port === 'string') {
-        return parseInt(port)
-    } else {
-        return port
-    }
-}
+	if (typeof port === 'string') {
+		return parseInt(port);
+	} else {
+		return port;
+	}
+};
 
-export const consolePrint = (
-    data: Array<string> | string,
-): Promise<winston.Logger> => {
-    return new Promise((resolve, reject) => {
-        if (Array.isArray(data)) {
-            return data.forEach(d => {
-                resolve(
-                    logger('Server Info').log({ level: 'info', message: d }),
-                )
-            })
-        } else if (typeof data === 'string') {
-            return resolve(
-                logger('Server Info').log({ level: 'info', message: data }),
-            )
-        } else {
-            return reject(
-                'Arguments need to be a string or an array of strings',
-            )
-        }
-    })
-}
+export const consolePrint = (data: Array<string> | string): Promise<winston.Logger> => {
+	return new Promise((resolve, reject) => {
+		if (Array.isArray(data)) {
+			return data.forEach((d) => {
+				resolve(logger('Server Info').log({ level: '5', message: d }));
+			});
+		} else if (typeof data === 'string') {
+			return resolve(logger('Server Info').log({ level: '5', message: data }));
+		} else {
+			return reject('Arguments need to be a string or an array of strings');
+		}
+	});
+};
 
 export const genResolvers = (path: string): IResolvers[] =>
-    glob.sync(path).map(resolver => require(resolver))
+	glob.sync(path).map((resolver) => require(resolver));
