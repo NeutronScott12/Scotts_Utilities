1e3276485b0f514789986effbb5d5c07bbad77bb
Scott Daniel <scottberry91@gmail.com>
2018-12-11 17:46:30 +0000

    0.0.8

--- src/logging/index.ts
+++ src/logging/index.ts
@@ -1,15 +1,15 @@
-import * as winston from 'winston';
-import * as logform from 'logform';
+import * as winston from 'winston'
+import * as logform from 'logform'
 
-const { combine, timestamp, label, prettyPrint } = logform.format;
+const { combine, timestamp, label, prettyPrint } = logform.format
 
 export const logger = (loggerLabel: string): winston.Logger => {
-	// const myFormat = printf((info) => {
-	// 	return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
-	// });
+    // const myFormat = printf((info) => {
+    // 	return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
+    // });
 
-	return winston.createLogger({
-		format: combine(label({ loggerLabel }), timestamp(), prettyPrint()),
-		transports: [ new winston.transports.Console() ]
-	});
-};
+    return winston.createLogger({
+        format: combine(label({ loggerLabel }), timestamp(), prettyPrint()),
+        transports: [new winston.transports.Console()],
+    })
+}
