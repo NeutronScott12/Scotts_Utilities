518bd6e0798f6a2936984f0e5670766229119875
Scott Daniel <scottberry91@gmail.com>
2018-12-13 19:49:13 +0000

    added file upload both local and s3

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,40 +1,42 @@
-import winston = require('winston')
-import * as glob from 'glob'
-import { IResolvers } from 'graphql-tools/dist/Interfaces'
-import { logger } from '../logging'
+import winston = require('winston');
+import * as glob from 'glob';
+import { IResolvers } from 'graphql-tools/dist/Interfaces';
+import { logger } from '../logging';
 
 export const sluggify = (word: string): string =>
-    word.replace(/ /g, '-').toLowerCase()
+	word.trim().replace(/ /g, '-').toLowerCase();
 
-export const normalisePort = (port: number | string): number => {
-    if (typeof port === 'string') {
-        return parseInt(port)
-    } else {
-        return port
-    }
-}
+export const normalisePort = (port: number | string): number | boolean => {
+	if (typeof port === 'string') {
+		return parseInt(port);
+	} else if (port >= 0) {
+		return port;
+	} else {
+		return false;
+	}
+};
 
 export const consolePrint = (
-    data: Array<string> | string,
+	data: Array<string> | string
 ): Promise<winston.Logger> => {
-    return new Promise((resolve, reject) => {
-        if (Array.isArray(data)) {
-            return data.forEach(d => {
-                resolve(
-                    logger('Server Info').log({ level: 'info', message: d }),
-                )
-            })
-        } else if (typeof data === 'string') {
-            return resolve(
-                logger('Server Info').log({ level: 'info', message: data }),
-            )
-        } else {
-            return reject(
-                'Arguments need to be a string or an array of strings',
-            )
-        }
-    })
-}
+	return new Promise((resolve, reject) => {
+		if (Array.isArray(data)) {
+			return data.forEach((d) => {
+				resolve(
+					logger('Server Info').log({ level: 'info', message: d })
+				);
+			});
+		} else if (typeof data === 'string') {
+			return resolve(
+				logger('Server Info').log({ level: 'info', message: data })
+			);
+		} else {
+			return reject(
+				'Arguments need to be a string or an array of strings'
+			);
+		}
+	});
+};
 
 export const genResolvers = (path: string): IResolvers[] =>
-    glob.sync(path).map(resolver => require(resolver))
+	glob.sync(path).map((resolver) => require(resolver));
---
+++ src/image/index.ts
@@ -0,0 +1,74 @@
+import * as path from 'path';
+import * as shortid from 'shortid';
+import { S3 } from 'aws-sdk';
+
+import { createWriteStream, WriteStream } from 'fs';
+
+interface S3ImageResponse {
+	filename: string;
+	mimetype: string;
+	encoding: string;
+	key: string;
+	url: string;
+	ETag: string;
+}
+
+export const S3imageUpload = async (
+	s3: S3,
+	bucket: string,
+	apolloUpload: any,
+	folder: string
+): Promise<S3ImageResponse | Error> => {
+	const { stream, filename, mimetype, encoding } = await apolloUpload;
+	const key = folder + '/' + shortid.generate() + '-' + filename;
+
+	if (s3) {
+		const response = await s3
+			.upload({
+				Bucket: bucket,
+				Key: key,
+				ACL: 'public-read',
+				Body: stream
+			})
+			.promise();
+
+		return {
+			filename,
+			mimetype,
+			encoding,
+			key: response.Key,
+			url: response.Location,
+			ETag: response.ETag
+		};
+	}
+
+	return new Error('Please add a S3 Client');
+};
+
+const processUpload = async (
+	stream: WriteStream,
+	mimetype: string,
+	path: string
+): Promise<any> => {
+	const extension = mimetype.split('/')[1];
+	const id = `${shortid.generate()}.${extension}`;
+
+	return new Promise((resolve, reject) => {
+		stream
+			.pipe(createWriteStream(path))
+			.on('finish', () => resolve({ id, path }))
+			.on('error', reject);
+	});
+};
+
+export const localUpload = async (
+	apolloUpload: any,
+	userPath: string
+): Promise<number | string> => {
+	userPath = userPath ? userPath : path.join(__dirname);
+
+	const { stream, mimetype } = await apolloUpload;
+	const { id } = await processUpload(stream, mimetype, userPath);
+
+	return id;
+};
