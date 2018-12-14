8cb3745cd0c16955c896cddcc6c728f562042325
Scott Daniel <scottberry91@gmail.com>
2018-12-14 17:54:49 +0000

    0.0.11

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,28 +1,28 @@
-import { generateNamespace } from '@gql2ts/from-schema';
-import { importSchema } from 'graphql-import';
-import * as fs from 'fs';
+import { generateNamespace } from '@gql2ts/from-schema'
+import { importSchema } from 'graphql-import'
+import * as fs from 'fs'
 
-import { logger } from '..';
+import { logger } from '..'
 
 export const createTypesFromSchema = (
-	namespace: string,
-	schemaPath: string,
-	destintation: string
+    namespace: string,
+    schemaPath: string,
+    destintation: string,
 ) => {
-	const typescriptTypes = generateNamespace(
-		namespace,
-		importSchema(schemaPath)
-	);
+    const typescriptTypes = generateNamespace(
+        namespace,
+        importSchema(schemaPath),
+    )
 
-	fs.writeFile(
-		destintation,
-		typescriptTypes,
-		(err: NodeJS.ErrnoException) => {
-			if (err)
-				logger('Create Types').error({
-					level: '5',
-					message: err.message
-				});
-		}
-	);
-};
+    fs.writeFile(
+        destintation,
+        typescriptTypes,
+        (err: NodeJS.ErrnoException) => {
+            if (err)
+                logger('Create Types').error({
+                    level: '5',
+                    message: err.message,
+                })
+        },
+    )
+}
