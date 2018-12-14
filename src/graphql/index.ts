cf392ec92e8020de2adbb221fd7da3be3517bcd8
Scott Daniel <scottberry91@gmail.com>
2018-12-14 17:48:46 +0000

    Changes gen resolvers and added ability to create types from schema.graphql

---
+++ src/graphql/index.ts
@@ -0,0 +1,28 @@
+import { generateNamespace } from '@gql2ts/from-schema';
+import { importSchema } from 'graphql-import';
+import * as fs from 'fs';
+
+import { logger } from '..';
+
+export const createTypesFromSchema = (
+	namespace: string,
+	schemaPath: string,
+	destintation: string
+) => {
+	const typescriptTypes = generateNamespace(
+		namespace,
+		importSchema(schemaPath)
+	);
+
+	fs.writeFile(
+		destintation,
+		typescriptTypes,
+		(err: NodeJS.ErrnoException) => {
+			if (err)
+				logger('Create Types').error({
+					level: '5',
+					message: err.message
+				});
+		}
+	);
+};
