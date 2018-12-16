043426b774e811cfe578403b2ba0985477d27e5a
Scott Daniel <scottberry91@gmail.com>
2018-12-16 17:12:05 +0000

    Chagned consolepint and added more types to auth functions

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,54 +1,72 @@
-import { sign, verify, decode } from 'jsonwebtoken'
-import { hash, compare } from 'bcryptjs'
+import {
+	sign,
+	verify,
+	decode,
+	DecodeOptions,
+	VerifyOptions,
+	SignOptions
+} from 'jsonwebtoken';
+import { hash, compare } from 'bcryptjs';
 
 export const createToken = (
-    data: string | {} | [],
-    secret: string,
-    expiresIn: string | number | '' = '',
-    refreshSecret?: string,
-    refreshData?: string | {} | [],
+	data: string | {} | [],
+	secret: string,
+	options: SignOptions,
+	refreshSecret?: string,
+	refreshData?: string | {} | []
 ): string[] => {
-    try {
-        const token = sign(data, secret, { expiresIn })
+	try {
+		const token = sign(data, secret, options);
 
-        if (refreshSecret && refreshData) {
-            const refreshToken: string = sign(refreshData, refreshSecret, {
-                expiresIn: '7d',
-            })
-            return [token, refreshToken]
-        }
+		if (refreshSecret && refreshData) {
+			const refreshToken: string = sign(
+				refreshData,
+				refreshSecret,
+				options
+			);
+			return [ token, refreshToken ];
+		}
 
-        return [token]
-    } catch (error) {
-        return error
-    }
-}
+		return [ token ];
+	} catch (error) {
+		return error;
+	}
+};
 
-export const verifyToken = (token: string, secret: string): string | {} => {
-    return verify(token, secret)
-}
+export const verifyToken = (
+	token: string,
+	secret: string,
+	options?: VerifyOptions
+): string | {} => {
+	return verify(token, secret, options);
+};
 
 export const hashPassword = async (
-    password: string,
-    salt: number,
+	password: string,
+	salt: number
 ): Promise<string> => {
-    return await hash(password, salt)
-}
+	return await hash(password, salt);
+};
 
 export const decodeToken = (
-    token: string,
+	token: string,
+	options?: DecodeOptions
 ):
-    | string
-    | {
-          [key: string]: any
-      }
-    | null => {
-    return decode(token)
-}
+	| string
+	| {
+			[key: string]: any;
+		}
+	| null => {
+	if (typeof token === 'string') {
+		return decode(token, options);
+	} else {
+		return new Error('Please enter a string based token');
+	}
+};
 
 export const comparePassword = async (
-    password: string,
-    userPassword: string,
+	password: string,
+	userPassword: string
 ): Promise<boolean> => {
-    return await compare(password, userPassword)
-}
+	return await compare(password, userPassword);
+};
