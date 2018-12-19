872ea0359df3b9884646696fb77d08cea7436855
Scott Daniel <scottberry91@gmail.com>
2018-12-19 17:38:17 +0000

    0.0.12

--- src/mailing/__tests__/test.ts
+++ src/mailing/__tests__/test.ts
@@ -1,16 +1,16 @@
-import { generateEmailLink } from '..';
+import { generateEmailLink } from '..'
 
 describe('Mailing Test', () => {
-	test('Generate Email Link', () => {
-		const link = generateEmailLink(
-			{
-				id: 1,
-				username: 'bob',
-				email: 'bob@example.com'
-			},
-			'http://localhost:4000'
-		);
+    test('Generate Email Link', () => {
+        const link = generateEmailLink(
+            {
+                id: 1,
+                username: 'bob',
+                email: 'bob@example.com',
+            },
+            'http://localhost:4000',
+        )
 
-		expect(link).toBeDefined();
-	});
-});
+        expect(link).toBeDefined()
+    })
+})
